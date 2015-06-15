cd32eff6a70a5839c89ef7a3ece05da287496bb4
shlomikalfa <shlomikalfa@yahoo.com>
2015-06-16 02:50:25 +0300

    Product page - adds to cart now.

--- libs/API/ebay/api_ebay_utils.php
+++ libs/API/ebay/api_ebay_utils.php
@@ -9,27 +9,37 @@
 abstract class ebay_Utils{
 
     static public function getEbayPicture($link, $size){
-      // h - height, w - width, wh - either, exactsize, original.
-      $sizes = array(
-          "400h"          => "1",
-          "200h"          => "2",
-          "800h"          => "3",
-          "480h"          => "4",
-          "75h"           => "5",
 
-          "70wh"           => "6",
-          "150wh"           => "7",
+        // h - height, w - width, wh - either, fb - fit-box, exactsize, original.
+        $sizes = array(
+            "400h"          => "1",
+            "200h"          => "2",
+            "800h"          => "3",
+            "480h"          => "4",
+            "75h"           => "5",
 
-          "200x150"           => "9",
-          "original"           => "10",
-          "310x90"           => "11",
+            "42w"           => "13",
+            "60w"           => "28",
+            "300w"          => "29",
 
-          "310x90"           => "12",
+            "60wh"           => "22",
+            "64wh"           => "14",
+            "70wh"           => "6",
+            "80wh"           => "17",
+            "150wh"           => "7",
+            "400wh"           => "19",
+            "500wh"           => "12",
 
+            "200x150"           => "9",
+            "original"           => "10",
+            "310x90"           => "11",
+            "400x300"         => "16",
+
+            "125x225fb"           => "15",
       );
-      $regexp = "/\\$\\_\\d+\\.JPG.+/i";
-      $replace = "\$_".$sizes[$size].".JPG";
-      return preg_replace($regexp, $replace, $link, 1);
+        $regexp = "/\\$\\_\\d+\\.JPG.+/i";
+        $replace = "\$_".$sizes[$size].".JPG";
+        return preg_replace($regexp, $replace, $link, 1);
     }
 
     static public function getDeliveryTime($time){
