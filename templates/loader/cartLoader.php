734ec2a42a20d1c1bb73b58f36412d7e0e36246a
shlomikalfa <shlomikalfa@yahoo.com>
2015-07-04 21:08:43 +0300

    Responsiveness-Ness added :)

--- templates/loader/cartLoader.php
+++ templates/loader/cartLoader.php
@@ -3,7 +3,7 @@
 $checkoutPage = get_permalink(get_option("cs_checkout_p_id"));
 if (!$checkoutPage){Utils::adminPreECHO("Can't get checkout page id", "cartLoader.php ERROR:: ");}
 
-$referredFrom = $_SERVER["HTTP_REFERER"];
+$referredFrom = (isset($_SERVER["HTTP_REFERER"]) && !empty($_SERVER["HTTP_REFERER"]))? $_SERVER["HTTP_REFERER"] : esc_url( home_url( '/' ) );
 
 $scope = array(
     "checkoutPage" => $checkoutPage,
